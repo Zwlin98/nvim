d796abb480b5894875f24f9a9f006df08c4c6420
Zwlin <zwlin98@outlook.com>
2023-09-06 19:12:24 +0800

    feat: neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -5,6 +5,7 @@ if vim.g.neovide then
 
     vim.opt.guifont = "FiraCode Nerd Font:h13"
     vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_transparency = 0.95
 
     vim.g.neovide_input_macos_alt_is_meta = true
     vim.g.neovide_cursor_vfx_mode = "railgun"
