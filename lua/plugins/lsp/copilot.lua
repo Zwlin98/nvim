0c3eb8e78a644d2ad41a453ea47d62ddb1751636
Zwlin <zwlin98@outlook.com>
2023-09-08 17:48:23 +0800

    feat: copilot

--- lua/plugins/lsp/copilot.lua
+++ lua/plugins/lsp/copilot.lua
@@ -1,7 +1,16 @@
+local rikka = require("rikka")
+local config = function()
+    vim.g.copilot_filetypes = {
+        registers = 0,
+    }
+
+    vim.g.copilot_no_tab_map = true
+    rikka.setKeymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, script = true, expr = true, replace_keycodes = false }
+    )
+end
+
 return {
     "github/copilot.vim",
     event = "VeryLazy",
-    config = function()
-        local rikka = require("rikka")
-    end,
+    config = config,
 }
