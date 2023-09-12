7f6250d9d5f17b7ac52426045206e2f5662a1791
Zwlin <zwlin98@outlook.com>
2023-09-12 10:08:43 +0800

    refactor: classification

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
     { import = "plugins.theme" },
     { import = "plugins.common" },
+    { import = "plugins.ui" },
     { import = "plugins.git" },
     { import = "plugins.lsp" },
     { import = "plugins.treesitter" },
