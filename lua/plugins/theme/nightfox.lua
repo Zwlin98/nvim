81c4041712955e113130da779b8b40af210441b7
Zwlin <zwlin98@outlook.com>
2023-09-19 13:18:06 +0800

    feat(theme): more consistent

--- lua/plugins/theme/nightfox.lua
+++ lua/plugins/theme/nightfox.lua
@@ -2,21 +2,33 @@ return {
     "EdenEast/nightfox.nvim",
     priority = 10000,
     config = function()
+        local palettes = {
+            nordfox = {
+                bg0  = "#2e3440", -- Dark bg (status line and float)
+                bg1  = "#2e3440", -- Default bg
+                bg2  = "#39404f", -- Lighter bg (colorcolm folds)
+                bg3  = "#444c5e", -- Lighter bg (cursor line)
+                bg4  = "#5a657d", -- Conceal, border fg
+
+                fg0  = "#c7cdd9", -- Lighter fg
+                fg1  = "#cdcecf", -- Default fg
+                fg2  = "#abb1bb", -- Darker fg (status line)
+                fg3  = "#7e8188", -- Darker fg (line numbers, fold colums)
+
+                sel0 = "#5a657d", -- Popup bg, visual selection bg
+                sel1 = "#4f6074", -- Popup sel bg, search bg
+            }
+        }
         local modules = {
             leap = {
                 enable = false,
             }
         }
-        local groups = {
-            nordfox = {
-                CursorLine = { bg = "#3a3944" },
-            }
-        }
         require('nightfox').setup({
+            palettes = palettes,
             options = {
                 modules = modules,
             },
-            groups = groups
         })
         -- load the colorscheme here
         vim.cmd([[colorscheme nordfox]])
