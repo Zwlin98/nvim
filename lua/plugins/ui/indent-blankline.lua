659654a6f24e1c040d9f9a1c6fac7c7550e3afb4
Zwlin <zwlin98@outlook.com>
2023-09-20 14:24:04 +0800

    feat(ui): ibl

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -33,14 +33,21 @@ return {
         rikka.createAutocmd({ "BufEnter", "BufWinEnter" }, {
             pattern = "*",
             callback = function(ev)
-                local cfg = vim.api.nvim_win_get_config(0)
-                if cfg and cfg.relative ~= '' then
-                    ibl.update { enabled = false }
+                local function check()
+                    local cfg = vim.api.nvim_win_get_config(0)
+                    if cfg and cfg.relative ~= '' then
+                        ibl.update { enabled = false }
+                    end
+                    if ev.buf and rikka.isBigFile(ev.buf) then
+                        ibl.update { enabled = false }
+                    else
+                        ibl.update { enabled = true }
+                    end
                 end
-                if ev.buf and rikka.isBigFile(ev.buf) then
+                local ok = pcall(check)
+                if not ok then
+                    vim.notify("Failed to check indent-blankline", vim.log.levels.ERROR)
                     ibl.update { enabled = false }
-                else
-                    ibl.update { enabled = true }
                 end
             end
         })
