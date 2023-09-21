cb87d43ab1d3c8a06ae090c5ba3935bd675a4edb
Zwlin <zwlin98@outlook.com>
2023-09-21 21:04:09 +0800

    feat(git): git blame

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -1,4 +1,10 @@
 return {
     "tpope/vim-fugitive",
     event = "VeryLazy",
+    config = function()
+        local rikka = require("rikka")
+        rikka.createCommand("Gb", function()
+            vim.cmd("Git blame --date=human")
+        end, {})
+    end
 }
