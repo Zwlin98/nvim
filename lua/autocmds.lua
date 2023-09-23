39f05b40260808f640263b24f07341cdfc44a793
Zwlin <zwlin98@outlook.com>
2023-09-23 20:35:17 +0800

    feat(edit): disable auto comment

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,3 +3,7 @@ local rikka = require("rikka")
 rikka.createAutocmd({ "VimEnter" }, {
     command = "clearjumps",
 })
+
+rikka.createAutocmd({ "FileType" }, {
+    command = "set formatoptions-=ro"
+})
