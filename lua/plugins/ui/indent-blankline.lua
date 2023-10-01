980713dd2e51a6faa8fa933dfbccf75d4b9fc3ae
Zwlin <zwlin98@outlook.com>
2023-10-01 21:36:46 +0800

    feat(ui): ibl update to master

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,58 +1,41 @@
--- Highlight group from rainbow-delimiters
-local highlight = {
-    "RainbowDelimiterRed",
-    "RainbowDelimiterYellow",
-    "RainbowDelimiterBlue",
-    "RainbowDelimiterOrange",
-    "RainbowDelimiterGreen",
-    "RainbowDelimiterViolet",
-    "RainbowDelimiterCyan",
-}
-
 return {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
-    branch = "v3",
-    opts = {
-        indent = {
-            char = "¦",
-            tab_char = "¦",
-        },
-        scope = {
-            enabled = true,
-            highlight = highlight,
-        },
-    },
-    config = function(_, opts)
-        local ibl = require("ibl")
-
-        ibl.setup(opts)
-
-        local rikka = require("rikka")
-
-        rikka.createAutocmd({ "BufEnter", "BufWinEnter" }, {
-            pattern = "*",
-            callback = function(ev)
-                local function check()
-                    local cfg = vim.api.nvim_win_get_config(0)
-                    if cfg and cfg.relative ~= '' then
-                        ibl.update { enabled = false }
-                    end
-                    if ev.buf and rikka.isBigFile(ev.buf) then
-                        ibl.update { enabled = false }
-                    else
-                        ibl.update { enabled = true }
-                    end
-                end
-                local ok = pcall(check)
-                if not ok then
-                    vim.notify("Failed to check indent-blankline", vim.log.levels.ERROR)
-                    ibl.update { enabled = false }
-                end
-            end
-        })
-
+    config = function()
+        local highlight = {
+            "RainbowRed",
+            "RainbowYellow",
+            "RainbowBlue",
+            "RainbowOrange",
+            "RainbowGreen",
+            "RainbowViolet",
+            "RainbowCyan",
+        }
         local hooks = require "ibl.hooks"
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        end)
+
+        vim.g.rainbow_delimiters = { highlight = highlight }
+        require("ibl").setup {
+            scope = {
+                highlight = highlight
+            },
+            indent = {
+                char = "¦",
+                tab_char = "¦",
+            },
+        }
+
         hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
+
 }
