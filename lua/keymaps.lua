d0c8133ea316226e046eff1aa7d517d686df5142
Zwlin <zwlin98@outlook.com>
2023-10-07 11:59:07 +0800

    feat(keymap): add tabclose

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,4 +9,5 @@ rikka.setKeymap("v", "<M-h>", ":nohlsearch<CR>", { desc = "No Highlight" })
 rikka.setKeymap('n', '[q', vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
 rikka.setKeymap('n', ']q', vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
 
-rikka.setKeymap("n", "<M-c>", ":close<CR>", { desc = "Close" })
+rikka.setKeymap("n", "<M-c>", ":close<CR>", { desc = "Close Window" })
+rikka.setKeymap("n", "<M-x>", ":tabclose<CR>", { desc = "Close Tab" })
