86790d62fa4e87aa2cde1a05e708441545a08841
Zwlin <zwlin98@outlook.com>
2023-10-07 11:58:45 +0800

    feat(cmp): optimize keybindings

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -85,24 +85,39 @@ return {
             mapping = {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
-                        if has_words_before() then
-                            cmp.confirm({ select = false })
-                        else
+                        local entry = cmp.get_selected_entry()
+                        if not entry then
                             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        else
+                            cmp.confirm()
                         end
                     elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
-                ["<CR>"] = cmp.mapping(function(fallback)
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
-                        cmp.confirm({ select = true })
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
+                ["<CR>"] = cmp.mapping({
+                    i = function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ select = true })
+                        else
+                            fallback()
+                        end
+                    end,
+                    s = cmp.mapping.confirm({ select = true }),
+                }),
                 ["<Down>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
