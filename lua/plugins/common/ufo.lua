43417e1fdc46bd1940b251efddace929a6691b44
Zwlin <zwlin98@outlook.com>
2023-10-13 13:30:16 +0800

    chore: disable foldcolumn

--- lua/plugins/common/ufo.lua
+++ lua/plugins/common/ufo.lua
@@ -5,7 +5,7 @@ return {
         "kevinhwang91/promise-async",
     },
     config = function()
-        vim.o.foldcolumn = "1" -- '0' is not bad
+        vim.o.foldcolumn = "0" -- '0' is not bad
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
