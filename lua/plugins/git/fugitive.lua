fae3bf7d5a0ad3a264bf5e29018ea31b75166302
Zwlin <zwlin98@outlook.com>
2023-11-09 17:43:29 +0800

    feat(git): add git blame keymap

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -6,5 +6,7 @@ return {
         rikka.createCommand("Gb", function()
             vim.cmd("Git blame --date=short")
         end, { desc = "Git Blame" })
+
+        rikka.setKeymap("n", "<M-m>", ":Git blame  --date=short<CR>", { desc = "Git Blame" })
     end,
 }
