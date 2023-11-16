86eb8dd277d4c2a2ec854b80cf74976b6ba55051
Zwlin <zwlin98@outlook.com>
2023-11-16 15:23:29 +0800

    chore: remove nohl keymap, can be replaced by <C-l>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,9 +3,6 @@ local rikka = require("rikka")
 rikka.setKeymap("v", "<", "<gv", { desc = "Left Indent" })
 rikka.setKeymap("v", ">", ">gv", { desc = "Right Indent" })
 
-rikka.setKeymap("n", "<M-h>", ":nohlsearch<CR>", { desc = "No Highlight" })
-rikka.setKeymap("v", "<M-h>", ":nohlsearch<CR>", { desc = "No Highlight" })
-
 rikka.setKeymap("n", "[q", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
 rikka.setKeymap("n", "]q", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
 
