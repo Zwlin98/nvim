d59c539fe02fa42c78dbcc05fc5a902011272005
Zwlin <zwlin98@outlook.com>
2023-11-17 13:20:55 +0800

    feat(lsp): upgrade fidget

--- lua/plugins/lsp/fidget.lua
+++ lua/plugins/lsp/fidget.lua
@@ -1,8 +1,13 @@
 return {
     "j-hui/fidget.nvim",
-    tag = "legacy",
     event = "LspAttach",
-    opts = {
-        -- options
-    },
+    config = function()
+        local fidget = require("fidget")
+        fidget.setup()
+        vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
+            -- local client = vim.lsp.get_client_by_id(ctx.client_id)
+            local level = ({ "ERROR", "WARN", "INFO", "DEBUG" })[result.type]
+            fidget.notify(result.message, level)
+        end
+    end,
 }
