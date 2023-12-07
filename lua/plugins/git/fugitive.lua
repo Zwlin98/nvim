99d3b2695a79295f7c9a1f6e578075295ee629a4
Zwlin <zwlin98@outlook.com>
2023-12-07 13:35:08 +0800

    feat(git): support toggle git blame

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -3,11 +3,24 @@ return {
     event = "VeryLazy",
     config = function()
         local rikka = require("rikka")
-        rikka.createCommand("Gb", function()
-            vim.cmd("Git blame --date=short")
-        end, { desc = "Git Blame" })
+
+        local function blameToggle()
+            local found = false
+            for _, winid in ipairs(vim.api.nvim_list_wins()) do
+                local bufnr = vim.api.nvim_win_get_buf(winid)
+                local filetype = vim.bo[bufnr].filetype
+                if filetype == "fugitiveblame" then
+                    vim.api.nvim_win_close(winid, true)
+                    found = true
+                    break
+                end
+            end
+            if not found then
+                vim.cmd("Git blame --date=short")
+            end
+        end
 
         -- aka: who write this code? leader-w(ho)
-        rikka.setKeymap("n", "<leader>w", ":Git blame  --date=short<CR>", { desc = "Git Blame" })
+        rikka.setKeymap("n", "<leader>w", blameToggle, { desc = "Git Blame Toggle" })
     end,
 }
