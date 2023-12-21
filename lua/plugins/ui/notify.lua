ed1903fedfa5f6c527df98fd88381890aeb27825
Zwlin <zwlin98@outlook.com>
2023-12-21 17:48:33 +0800

    feat(ui): notify support transparents

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -5,6 +5,7 @@ return {
         local notify = require("notify")
         notify.setup({
             timeout = 1000,
+            background_colour = "#000000"
         })
         vim.notify = notify
     end,
