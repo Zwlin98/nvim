05c1f8b2b283dab4f0e3aa109533bed647ca88b9
Zwlin <zwlin98@outlook.com>
2024-01-08 20:05:13 +0800

    feat(ui): ufo virtual text

--- lua/plugins/common/ufo.lua
+++ lua/plugins/common/ufo.lua
@@ -24,7 +24,7 @@ return {
             ["lua.txt"] = "indent",
         }
 
-        local function suffix(filename)
+        local function extension(filename)
             for k, v in pairs(suffixMap) do
                 if string.sub(filename, -string.len(k)) == k then
                     return v
@@ -50,10 +50,39 @@ return {
                 end)
         end
 
+        local handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    local hlGroup = chunk[2]
+                    table.insert(newVirtText, { chunkText, hlGroup })
+                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    -- str width returned from truncate() may less than 2nd argument, need padding
+                    if curWidth + chunkWidth < targetWidth then
+                        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+                    end
+                    break
+                end
+                curWidth = curWidth + chunkWidth
+            end
+            table.insert(newVirtText, { suffix, "MoreMsg" })
+            return newVirtText
+        end
+
         ufo.setup({
+            fold_virt_text_handler = handler,
             provider_selector = function(bufnr, filetype, buftype)
                 local filename = vim.api.nvim_buf_get_name(bufnr)
-                return suffix(filename) or ftMap[filetype] or customizeSelector
+                return extension(filename) or ftMap[filetype] or customizeSelector
             end,
         })
 
