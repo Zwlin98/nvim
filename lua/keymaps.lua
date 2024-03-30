859d39c450b1231afb3987525c309c19c0da2d33
Zwlin <zwlin98@outlook.com>
2024-03-30 21:38:02 +0800

    feat(keymap): remove q:

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,3 +5,5 @@ rikka.setKeymap("v", ">", ">gv", { desc = "Right Indent" })
 
 rikka.setKeymap("n", "<M-c>", ":close<CR>", { desc = "Close Window" })
 rikka.setKeymap("n", "<M-x>", ":tabclose<CR>", { desc = "Close Tab" })
+
+rikka.setKeymap("n", "q:", ":q<CR>", { desc = "Quit" }) -- q: is useless and annoying
