dff347870b29607ca6a67873fe78387d88f942bd
Zwlin <zwlin98@outlook.com>
2024-05-17 18:03:50 +0800

    feat: v0.10 refactor lsp config

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -23,34 +23,37 @@ return {
 
         -- Override floating window settings
         local lspUtilOpenFloatingPreview = vim.lsp.util.open_floating_preview
+        ---@diagnostic disable-next-line: duplicate-set-field
         function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
             opts = opts or {}
             opts.border = opts.border or rikka.border
-            local bufnr, winnr = lspUtilOpenFloatingPreview(contents, syntax, opts, ...)
-            if bufnr then
-                vim.api.nvim_win_set_option(winnr, "winblend", 20)
-            end
+            lspUtilOpenFloatingPreview(contents, syntax, opts, ...)
+            -- local bufnr, winnr = lspUtilOpenFloatingPreview(contents, syntax, opts, ...)
+            -- if bufnr then
+            --     vim.api.nvim_win_set_var(winnr, "winblend", 10)
+            -- end
         end
 
         rikka.createAutocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(env)
-                local buffer = env.buf
+            callback = function(args)
+                local client = vim.lsp.get_client_by_id(args.data.client_id)
+                if not client then
+                    return
+                end
+
+                if client.supports_method("textDocument/inlayHints") then
+                    vim.lsp.inlay_hint.enable()
+                end
+
+                local buffer = args.buf
                 rikka.setBufKeymap(buffer, "n", "gr", telescope.lsp_references, { desc = "Check references under cursor" })
                 rikka.setBufKeymap(buffer, "n", "gd", telescope.lsp_definitions, { desc = "Check definitions under cursor" })
                 rikka.setBufKeymap(buffer, "n", "gi", telescope.lsp_implementations, { desc = "Check implementations under cursor" })
-                rikka.setBufKeymap(buffer, "n", "H", vim.lsp.buf.hover, { desc = "Lsp Hover" })
                 rikka.setBufKeymap(buffer, "n", "ga", actions_preview.code_actions, { desc = "Code Actions" })
                 rikka.setBufKeymap(buffer, "n", "<space>r", vim.lsp.buf.rename, { desc = "Rename" })
 
                 rikka.setBufKeymap(buffer, "n", "gs", telescope.lsp_document_symbols, { desc = "Document Symbols" })
-
-                rikka.setBufKeymap(buffer, "n", "[d", function()
-                    vim.diagnostic.goto_prev({ float = false })
-                end, { desc = "Previous Diagnostic" })
-                rikka.setBufKeymap(buffer, "n", "]d", function()
-                    vim.diagnostic.goto_next({ float = false })
-                end, { desc = "Next Diagnostic" })
             end,
         })
 
@@ -69,7 +72,6 @@ return {
             "go",
             "lua",
             "markdown",
-            "php",
             "rust",
             "python",
         }
