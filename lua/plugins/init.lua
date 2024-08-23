bca8da1970dfb052da1e7909c5872ca5776c811e
Zwlin <zwlin98@outlook.com>
2024-08-23 10:27:54 +0800

    feat(plugin): update lazy.nvim

--- lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-    { import = "plugins.theme" },
-    { import = "plugins.common" },
-    { import = "plugins.ui" },
-    { import = "plugins.git" },
-    { import = "plugins.lsp" },
-    { import = "plugins.treesitter" },
-}
