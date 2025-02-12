5877ecb85ad9d9e587e5214857339f263c53b5ef
Zwlin <zwlin98@outlook.com>
2025-02-12 10:32:59 +0800

    feat(imselect): using macism

--- lua/plugins/common/im-select.lua
+++ lua/plugins/common/im-select.lua
@@ -13,7 +13,7 @@ return {
             -- For Windows/WSL, default: "im-select.exe"
             -- For macOS, default: "im-select"
             -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
-            default_command = "im-select",
+            default_command = "macism",
 
             -- Restore the default input method state when the following events are triggered
             set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
