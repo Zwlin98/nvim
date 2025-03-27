056e0566a802e1d91807ba517703257f2422ab2e
Zwlin <zwlin98@outlook.com>
2025-03-27 08:54:22 +0800

    feat(color): change color for leap

--- lua/plugins/common/leap.lua
+++ lua/plugins/common/leap.lua
@@ -7,5 +7,18 @@ return {
         local rikka = require("rikka")
         rikka.setKeymap("n", "s", "<Plug>(leap)", { desc = "Leap" })
         rikka.setKeymap("n", "S", "<Plug>(leap-from-window)", { desc = "Leap from window" })
+
+        -- LeapLabelDimmed
+        -- LeapMatch
+        -- LeapLabel
+        local LeapColors = {
+            LeapLabelDimmed = { bg = rikka.color.lightGray, fg = rikka.color.white },
+            LeapMatch = { bg = rikka.color.orange, fg = rikka.color.black },
+            LeapLabel = { bg = rikka.color.green, fg = rikka.color.black },
+        }
+
+        for group, color in pairs(LeapColors) do
+            rikka.setHightlight(group, color)
+        end
     end,
 }
