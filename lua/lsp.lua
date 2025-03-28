cefa4985af9b7d904a0862d62d6e4e72602fdd53
Zwlin <zwlin98@outlook.com>
2025-03-28 11:41:28 +0800

    feat: refactor lsp config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,6 +2,7 @@ if vim.env.KITTY_SCROLLBACK_NVIM == "true" then
 else
     local rikka = require("rikka")
     local fzf = require("fzf-lua")
+
     vim.lsp.config("*", {
         root_markers = { ".git" },
     })
@@ -18,18 +19,54 @@ else
         },
     })
 
-    local function lspReferences()
-        fzf.lsp_references({
-            ignore_current_line = true,
-        })
-    end
-
-    local function lspDefinitions()
-        fzf.lsp_definitions({
-            sync = true,
-            jump1 = true,
-        })
-    end
+    local supportMethodSetups = {
+        ["textDocument/completion"] = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if not client then
+                return
+            end
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+        end,
+        ["textDocument/codeAction"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "ga", fzf.lsp_code_actions, { desc = "Code Actions" })
+        end,
+        ["textDocument/definition"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "gd", function()
+                fzf.lsp_definitions({
+                    sync = true,
+                    jump1 = true,
+                })
+            end, { desc = "Check definitions under cursor" })
+        end,
+        ["textDocument/hover"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+        end,
+        ["textDocument/inlayHints"] = function(args)
+            vim.lsp.inlay_hint.enable()
+        end,
+        ["textDocument/implementation"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "gi", fzf.lsp_implementations, { desc = "Check implementations under cursor" })
+        end,
+        ["callHierarchy/incomingCalls"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "g(", fzf.lsp_incoming_calls, { desc = "Check incoming calls under cursor" })
+        end,
+        ["callHierarchy/outgoingCalls"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "g)", fzf.lsp_outgoing_calls, { desc = "Check outgoing calls under cursor" })
+        end,
+        ["textDocument/documentSymbol"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "gs", fzf.lsp_document_symbols, { desc = "Document Symbols" })
+        end,
+        ["textDocument/rename"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "<space>r", vim.lsp.buf.rename, { desc = "Rename" })
+        end,
+        ["textDocument/references"] = function(args)
+            rikka.setBufKeymap(args.buf, "n", "gr", function()
+                fzf.lsp_references({
+                    ignore_current_line = true,
+                })
+            end, { desc = "Check references under cursor" })
+        end,
+    }
 
     rikka.createAutocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -38,23 +75,16 @@ else
             if not client then
                 return
             end
-
-            if client:supports_method("textDocument/inlayHints") then
-                vim.lsp.inlay_hint.enable()
-            end
-            local buffer = args.buf
-            -- Unset 'omnifunc'
-            vim.bo[buffer].omnifunc = nil
-
-            rikka.setBufKeymap(buffer, "n", "gr", lspReferences, { desc = "Check references under cursor" })
-            rikka.setBufKeymap(buffer, "n", "gd", lspDefinitions, { desc = "Check definitions under cursor" })
-            rikka.setBufKeymap(buffer, "n", "gi", fzf.lsp_implementations, { desc = "Check implementations under cursor" })
-            rikka.setBufKeymap(buffer, "n", "ga", fzf.lsp_code_actions, { desc = "Code Actions" })
-            rikka.setBufKeymap(buffer, "n", "<space>r", vim.lsp.buf.rename, { desc = "Rename" })
-
-            rikka.setBufKeymap(buffer, "n", "gs", fzf.lsp_document_symbols, { desc = "Document Symbols" })
-
             vim.cmd([[nnoremap <nowait> gr gr]])
+
+            -- Unset 'omnifunc'
+            vim.bo[args.buf].omnifunc = nil
+
+            for method, func in pairs(supportMethodSetups) do
+                if client:supports_method(method) then
+                    func(args)
+                end
+            end
         end,
     })
 
