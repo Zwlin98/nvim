b491b49bcf6bdf71bac9bd0c33820c22d6c41d05
Zwlin <zwlin98@outlook.com>
2025-04-17 13:44:25 +0800

    feat: lua ls enable inlayhint

--- lsp/lua.lua
+++ lsp/lua.lua
@@ -11,11 +11,11 @@ return {
     -- Specific settings to send to the server. The schema for this is
     -- defined by the server. For example the schema for lua-language-server
     -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-    -- settings = {
-    --     Lua = {
-    --         runtime = {
-    --             version = "LuaJIT",
-    --         },
-    --     },
-    -- },
+    settings = {
+        Lua = {
+            hint = {
+                enable = true,
+            },
+        },
+    },
 }
