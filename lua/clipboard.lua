09b7ebae796fd50c675df3d7f055264e00d74798
Zwlin <zwlin98@outlook.com>
2025-04-25 09:48:53 +0800

    refactor: simplify clipboard OSC52 integration for remote non-tmux sessions

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -1,28 +1,8 @@
 local rikka = require("rikka")
 
 if rikka.isRemote() and not rikka.isTmux() then
-    local osc52 = require("vim.ui.clipboard.osc52")
-
     vim.o.clipboard = "unnamedplus"
-
-    local function paste()
-        return {
-            vim.fn.split(vim.fn.getreg(""), "\n"),
-            vim.fn.getregtype(""),
-        }
-    end
-
-    vim.g.clipboard = {
-        name = "OSC 52",
-        copy = {
-            ["+"] = osc52.copy("+"),
-            ["*"] = osc52.copy("*"),
-        },
-        paste = {
-            ["+"] = paste,
-            ["*"] = paste,
-        },
-    }
+    vim.g.clipboard = "osc52"
 else
     vim.o.clipboard = "unnamedplus"
 end
