89ff805dc258b183a565b0a563237364edea8e10
Zwlin <zwlin98@outlook.com>
2025-04-30 10:27:49 +0800

    feat: add custom config and highlight groups for render-markdown plugin

--- lua/plugins/ui/markdown.lua
+++ lua/plugins/ui/markdown.lua
@@ -1,5 +1,31 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    opts = {},
+    config = function()
+        local markdown = require("render-markdown")
+        local rikka = require("rikka")
+        markdown.setup({
+            heading = {
+                sign = false,
+                position = "inline",
+            },
+            code = {
+                width = "block",
+            },
+        })
+
+        local customHighlightGroup = {
+
+            RenderMarkdownH1Bg = { bg = nil },
+            RenderMarkdownH2Bg = { bg = nil },
+            RenderMarkdownH3Bg = { bg = nil },
+            RenderMarkdownH4Bg = { bg = nil },
+            RenderMarkdownH5Bg = { bg = nil },
+            RenderMarkdownH6Bg = { bg = nil },
+        }
+
+        vim.iter(customHighlightGroup):each(function(group, opts)
+            rikka.setHightlight(group, opts)
+        end)
+    end,
 }
