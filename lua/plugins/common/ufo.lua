8ee2332adcebe3fe51a3ad586fe61b377d654b23
Zwlin <zwlin98@outlook.com>
2025-06-24 13:32:07 +0800

    feat: add pyright LSP configuration and update ufo dependencies

--- lua/plugins/common/ufo.lua
+++ lua/plugins/common/ufo.lua
@@ -2,7 +2,6 @@ return {
     "kevinhwang91/nvim-ufo",
     event = "VeryLazy",
     dependencies = {
-        "nvim-lspconfig",
         "kevinhwang91/promise-async",
     },
     config = function()
