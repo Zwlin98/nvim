8b8e2b00f6364df77e29d3fb14bb02890a2b2cf0
Zwlin <zwlin98@outlook.com>
2025-08-22 15:25:26 +0800

    refactor: replace blink.pairs with nvim-autopairs for autopairing

--- lua/plugins/common/autopairs.lua
+++ lua/plugins/common/autopairs.lua
@@ -1,30 +1,5 @@
 return {
-    "saghen/blink.pairs",
-    version = "*", -- (recommended) only required with prebuilt binaries
-    -- download prebuilt binaries from github releases
-    dependencies = "saghen/blink.download",
-    opts = {
-        highlights = {
-            enabled = true,
-            groups = {
-                "BlinkPairsBlue", -- third level
-                "BlinkPairsOrange", -- second level
-                "BlinkPairsPurple", -- fisrt level
-            },
-        },
-    },
-    config = function(_, opts)
-        require("blink.pairs").setup(opts)
-
-        local rikka = require("rikka")
-        local customHighlightGroup = {
-            BlinkPairsBlue = { fg = "#8FBCBB", bg = nil },
-            BlinkPairsOrange = { fg = rikka.color.orange, bg = nil },
-            BlinkPairsPurple = { fg = rikka.color.violet, bg = nil },
-        }
-
-        for group, colorOpts in pairs(customHighlightGroup) do
-            rikka.setHightlight(group, colorOpts)
-        end
-    end,
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
 }
