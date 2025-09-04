164aaa0e8babf7f19a8db06c43c3d25257c02dd0
Zwlin <zwlin98@outlook.com>
2025-09-04 14:35:32 +0800

    feat: add code style customization for markdown rendering

--- lua/plugins/ui/markdown.lua
+++ lua/plugins/ui/markdown.lua
@@ -13,6 +13,7 @@ return {
             },
             code = {
                 width = "block",
+                highlight_border = false,
             },
         })
 
@@ -23,6 +24,11 @@ return {
             RenderMarkdownH4Bg = { fg = rikka.color.yellow, bg = "#434c5e", bold = true }, -- nord13 on nord2
             RenderMarkdownH5Bg = { fg = rikka.color.violet, bg = "#3b4252", bold = true }, -- nord15 on nord1
             RenderMarkdownH6Bg = { fg = rikka.color.orange, bg = "#434c5e", bold = true }, -- nord12 on nord2
+            RenderMarkdownCode = {},
+            RenderMarkdownCodeBorder = {},
+            RenderMarkdownCodeInfo = {},
+            RenderMarkdownCodeFallback = {},
+            RenderMarkdownCodeInline = {},
         }
         vim.iter(customHighlightGroup):each(function(group, opts)
             rikka.setHightlight(group, opts)
