3a671f2ef6d6cc4c6b66a684c7bc3b744ad5f48c
Zwlin <zwlin98@outlook.com>
2025-12-15 15:20:38 +0800

    feat: replace treesitter incremental selection with incr.nvim
    Treesitter no longer supports incremental selection in newer versions, so switch to the dedicated incr.nvim plugin to maintain this functionality.

---
+++ lua/plugins/common/incr.lua
@@ -0,0 +1,7 @@
+return {
+    "Zwlin98/incr.nvim",
+    opts = {
+        incr_key = "<CR>", -- increment selection key
+        decr_key = "<M-CR>", -- decrement selection key
+    },
+}
--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -1,50 +1,29 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
     config = function()
-        local rikka = require("rikka")
-        require("nvim-treesitter.configs").setup({
-            -- 安装 language parser
-            -- :TSInstallInfo 命令查看支持的语言
-            ensure_installed = {
-                "bash",
-                "c",
-                "go",
-                "json",
-                "kdl",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "proto",
-                "regex",
-                "rust",
-                "yaml",
-                "php",
-                "beancount",
-                "comment",
-                "vim",
-                "vimdoc",
-                "html",
-            },
-            -- 启用代码高亮功能
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-                -- disable = function(lang, buf)
-                --     return rikka.isBigFile(buf)
-                -- end,
-            },
-
-            indent = { enable = true },
-
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "<CR>",
-                    scope_incremental = "grc",
-                    node_decremental = "<M-CR>",
-                },
-            },
-        })
+        local treesitter = require("nvim-treesitter")
+        local wanted = {
+            "bash",
+            "c",
+            "go",
+            "json",
+            "kdl",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "proto",
+            "regex",
+            "rust",
+            "yaml",
+            "php",
+            "beancount",
+            "comment",
+            "vim",
+            "vimdoc",
+            "html",
+        }
+        treesitter.install(wanted)
     end,
 }
