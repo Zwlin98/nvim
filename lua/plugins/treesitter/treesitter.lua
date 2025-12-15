cc7ec6735d85194b91f921a18b40079b8810db08
Zwlin <zwlin98@outlook.com>
2025-12-15 17:01:48 +0800

    feat: customize treesitter setup with manual highlight and indent configuration

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -4,6 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
         local treesitter = require("nvim-treesitter")
+
         local wanted = {
             "bash",
             "c",
@@ -23,7 +24,33 @@ return {
             "vim",
             "vimdoc",
             "html",
+            "python",
         }
+
         treesitter.install(wanted)
+
+        local languages = {}
+        for _, lang in ipairs(wanted) do
+            languages[lang] = true
+        end
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("treesitter.setup", {}),
+            callback = function(args)
+                local buf = args.buf
+                local filetype = args.match
+
+                local language = vim.treesitter.language.get_lang(filetype) or filetype
+                if not languages[language] then
+                    return
+                end
+
+                -- replicate `highlight = { enable = true }`
+                vim.treesitter.start(buf, language)
+
+                -- replicate `indent = { enable = true }`
+                vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
     end,
 }
