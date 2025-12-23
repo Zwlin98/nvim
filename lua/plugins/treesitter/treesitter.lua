4b1a74c97144dd419116a6b7bfb9cedb343019b1
Zwlin <zwlin98@outlook.com>
2025-12-23 10:12:50 +0800

    feat: switch treesitter installation from stable to all parsers

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -5,12 +5,7 @@ return {
     config = function()
         local treesitter = require("nvim-treesitter")
 
-        treesitter.install("stable")
-
-        local languages = {}
-        for _, lang in ipairs(treesitter.get_available()) do
-            languages[lang] = true
-        end
+        treesitter.install("all")
 
         vim.api.nvim_create_autocmd("FileType", {
             group = vim.api.nvim_create_augroup("treesitter.setup", {}),
@@ -19,15 +14,9 @@ return {
                 local filetype = args.match
 
                 local language = vim.treesitter.language.get_lang(filetype) or filetype
-                if not languages[language] then
-                    return
+                if pcall(vim.treesitter.start, buf, language) then
+                    vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                 end
-
-                -- replicate `highlight = { enable = true }`
-                vim.treesitter.start(buf, language)
-
-                -- replicate `indent = { enable = true }`
-                vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end,
         })
     end,
