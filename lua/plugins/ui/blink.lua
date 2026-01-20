08fb4b56e95d2327a143fe3663543a56c5c2e51b
Zwlin <zwlin98@outlook.com>
2026-01-20 09:33:35 +0800

    refactor: remove lspkind and nvim-web-devicons dependencies for blink.cmp

--- lua/plugins/ui/blink.lua
+++ lua/plugins/ui/blink.lua
@@ -3,8 +3,6 @@ return {
     -- optional: provides snippets for the snippet source
     dependencies = {
         "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim",
-        "nvim-tree/nvim-web-devicons",
     },
 
     -- use a release tag to download pre-built binaries
@@ -43,44 +41,6 @@ return {
                     selection = { preselect = false, auto_insert = true },
                 },
                 documentation = { auto_show = false },
-                menu = {
-                    draw = {
-                        components = {
-                            kind_icon = {
-                                text = function(ctx)
-                                    local lspkind = require("lspkind")
-                                    local icon = ctx.kind_icon
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                                        if dev_icon then
-                                            icon = dev_icon
-                                        end
-                                    else
-                                        icon = lspkind.symbolic(ctx.kind, {
-                                            mode = "symbol",
-                                        })
-                                    end
-
-                                    return icon .. ctx.icon_gap
-                                end,
-
-                                -- Optionally, use the highlight groups from nvim-web-devicons
-                                -- You can also add the same function for `kind.highlight` if you want to
-                                -- keep the highlight groups in sync with the icons.
-                                highlight = function(ctx)
-                                    local hl = ctx.kind_hl
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                                        if dev_icon then
-                                            hl = dev_hl
-                                        end
-                                    end
-                                    return hl
-                                end,
-                            },
-                        },
-                    },
-                },
             },
 
             -- Default list of enabled providers defined so that you can extend it
